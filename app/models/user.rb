2039881d2ebf953d13a5a8355f0c139934f33e11
Taylor <twsmith89@gmail.com>
2013-10-08 11:48:10 -0500

    add has_many and belongs_to relationships to user and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :events
+  
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
